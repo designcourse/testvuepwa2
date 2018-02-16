fa8d4977e83d5697ceb76c22095913899c125124
Gary Simon <contact@designcourse.com>
2018-02-16 11:38:42 -0500

    Updates

---
+++ docs/precache-manifest.37e03af184bc748377dadce3914e388c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c17859096e5ef6c57d25",
+    "url": "/js/vendor.c1785909.js"
+  },
+  {
+    "revision": "ce28c628eea246b643c9",
+    "url": "/js/manifest.ce28c628.js"
+  },
+  {
+    "revision": "2129dac3c159d07206ff",
+    "url": "/js/app.2129dac3.js"
+  },
+  {
+    "revision": "829c008acb68979d4dad9f97e968e0d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2129dac3c159d07206ff",
+    "url": "/css/app.1b3ba563.css"
+  }
+];
